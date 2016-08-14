b1755bf96bd3487c6980cdfa02436910556e6420
stryku <stryku2393@gmail.com>
2016-08-14 23:40:50 +0200

    created basic ModelObserver declaration

---
+++ sources/inc/internals/model/ModelObserver.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+namespace Icyus
+{
+    namespace Model
+    {
+        class IModelObserver abstract
+        {
+        public:
+            ~IModelObserver() = default;
+
+        };
+    }
+}
