9d528c79500d56c504957d874fc047f9da7d8419
stryku <stryku2393@gmail.com>
2016-08-20 20:57:07 +0200

    changed communication pattern to request-reply. added logs to receiver

--- sources/src/internals/communication/FileReceiver.cpp
+++ sources/src/internals/communication/FileReceiver.cpp
@@ -1,5 +1,8 @@
 #include <internals/communication/FileReceiver.hpp>
 
+#include <internals/utils/log.hpp>
+#include <internals/communication/utils/helpers.hpp>
+
 namespace Icyus
 {
     namespace Communication
@@ -7,30 +10,33 @@ namespace Icyus
         FileReceiver::FileReceiver(zmq::context_t &ctx,
                                    std::function<void(size_t)> callaback) :
             context{ ctx },
-            socket{ context, zmq::socket_type::pull },
+            socket{ context, zmq::socket_type::rep },
             progressCallback{ callaback }
         {}
 
         void FileReceiver::receiveFile()
         {
+            constexpr auto ok{ "ok" };
             auto fileSize{ 0ull };
             auto alreadyReceivedBytes{ 0ull };
             auto currentlyReceived{ 0ull };
             zmq::message_t msg;
+            zmq::message_t okMsg{ 1 };
             auto fileName{ "out" };
             std::ofstream out(fileName, std::ios::binary);
-
             LOG("receiveFile. File will be written to " << fileName);
 
             socket.recv(&msg);
+            LOG("received file size: " << fileSize);
+            socket.send(okMsg);
+            LOG("send ok msg");
 
             fileSize = std::stoull(msg.str());
-
-            LOG("received file size: " << fileSize);
 
             while (alreadyReceivedBytes < fileSize)
             {
                 socket.recv(&msg);
+                socket.send(okMsg);
 
                 currentlyReceived = msg.size();
 
@@ -40,7 +46,7 @@ namespace Icyus
                 if (progressCallback)
                     progressCallback(alreadyReceivedBytes);
 
-                LOG("received file chunk. Received " << alreadyReceivedBytes << "/" << fileSize << "b (" << 100 * alreadyReceivedBytes / fileSize << "%)");
+                LOG("Received " << alreadyReceivedBytes << "/" << fileSize << "b (" << 100 * alreadyReceivedBytes / fileSize << "%)");
 
                 out.write(static_cast<const char*>(msg.data()), msg.size());
             }
@@ -50,11 +56,19 @@ namespace Icyus
 
         void FileReceiver::startListening(const std::string &address)
         {
-            if (receivingThread.joinable())
-                receivingThread.join();
+            LOG("Receiver start listening");
 
+            if (receivingThread.joinable())
+            {
+                LOG("receiving thread joinable. Joining.");
+                receivingThread.join();
+                LOG("Joining successfull");
+            }
+
+            LOG("Binding");
             socket.bind(address);
 
+            LOG("Starting receiving thread");
             receivingThread = std::thread{ [this] { receiveFile(); } };
         }
     }
